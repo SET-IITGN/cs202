269a5ad3262beb40e8b91a9804c2d6d365c1bf32
Shouvick Mondal <39446336+shouvick149@users.noreply.github.com>
2025-01-05 17:12:50 +0530

    Fixed indentation issues.

--- src.py
+++ src.py
@@ -1,8 +1,10 @@
 if __name__ == "__main__":
-		y=1;
-		if(y<0):
-			if (y>0): y=3;
-			else: y=0;
-			print(y);
+	y=1;
+	if(y<0):
+		if (y>0):
+			y=3;
+		else: 
+			y=0;
+	print(y);
 			
 assert(y==1)
