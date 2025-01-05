9e600a22bf1a8369306da56a29fb4b031cd15651
shouvick149 <shouvick.mondal.cemk@gmail.com>
2025-01-05 16:01:20 +0530

    minor edits.

--- src.c
+++ src.c
@@ -10,13 +10,12 @@ void copyMemory(char* destination, char* source, int numBytes) {
 }
 int main() {
     char *buffer;
-    char  *message = "Hello";
-    
-    int numBytes = strlen(message);
+    char message[] = "Hello, worlcome to CS202";
+	
+	int numBytes = strlen(message);
     buffer=(char*)malloc(numBytes+1);
     memset(buffer,'\0',numBytes+1);
-    if(numBytes<sizeof(buffer))
-    	copyMemory(buffer, message, numBytes);
+    copyMemory(buffer, message, numBytes);
     printf("numbytes=%d,Copied message: %s\n",numBytes, buffer);
     return 0;
 }
