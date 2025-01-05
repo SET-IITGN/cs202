5c208fc439dafd0cf8457a4df43f8adb9763afcf
shouvick149 <shouvick.mondal.cemk@gmail.com>
2025-01-05 12:16:48 +0530

    added checks

--- src.c
+++ src.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void copyMemory(char* destination, const char* source, int numBytes) {
-    for (int i = 0; i < numBytes; i++) {
+void copyMemory(char* destination, char* source, int numBytes) {
+    int i;
+    for (i = 0; i < numBytes; i++) {
         destination[i] = source[i];
     }
 }
 int main() {
-    char buffer[256];
-    const char* message = "Hello, world!";
-    int numBytes = sizeof(message);      
-    copyMemory(buffer, message, numBytes);
-    printf("Copied message: %s\n", buffer);
+    char *buffer;
+    char  *message = "Hello";
+    
+    int numBytes = strlen(message);
+    buffer=(char*)malloc(numBytes+1);
+    memset(buffer,'\0',numBytes+1);
+    if(numBytes<sizeof(buffer))
+    	copyMemory(buffer, message, numBytes);
+    printf("numbytes=%d,Copied message: %s\n",numBytes, buffer);
     return 0;
 }
 //edited here
